52c9ade2d00d6aef82f206a4ae750dd2438296b6
David Sterry <sterry.david@gmail.com>
2020-10-19 10:59:17 -0700

    add index and jscript files
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+	return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Pick a number."));
+const number2 = parseInt(prompt("And another number."))
+
+alert(add(number1, number2));
