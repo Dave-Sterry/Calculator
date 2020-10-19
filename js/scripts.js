2fea504810f4a4c71d9b65383d8c9f5a722d5540
David Sterry <sterry.david@gmail.com>
2020-10-19 11:17:25 -0700

    add mulit, subtract and divide functions
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,15 @@ function add(number1, number2) {
 const number1 = parseInt(prompt("Pick a number."));
 const number2 = parseInt(prompt("And another number."))
 
-alert(add(number1, number2));
+function sub(number1, number2) {
+  return number1 - number2;
+}
+
+function multi(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+alert(divide(number1, number2));
