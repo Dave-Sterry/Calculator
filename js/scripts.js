4f09bc7d3a5d94240895f161e6d28f84c6a9329c
David Sterry <sterry.david@gmail.com>
2020-10-22 14:48:54 -0700

    update html and scripts.js for new lesson
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -16,37 +16,11 @@ const divide = function(number1, number2) {
 
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function) {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator =$("input:radio[name=operator]:checked").val();
     const result = add(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#sub1").val());
-    const number2 = parseInt($("#sub2").val());
-    const result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#multi1").val());
-    const number2 = parseInt($("#multi2").val());
-    const result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#divide1").val());
-    const number2 = parseInt($("#divide2").val());
-    const result = divide(number1, number2);
-    $("#output").text(result);
-  });
-});
-
-
+    $('#output').text(result);
+  }
